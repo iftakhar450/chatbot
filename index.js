d54c126405ff611fd5c128687d5058550bdc0e53
IFTAKHAR <iftakhar45@yahoo.com>
2018-10-18 11:55:35 +0500

    port change

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const verifyWebhook = require('./verify-webhook');
 const app = express();
-
+var port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
     app.get('/', verifyWebhook);
 
-app.listen(5000, () => console.log('Express server is listening on port 5000'));
+app.listen(port, function() {
+        console.log('Our app is running on http://localhost:' + port);
+    });
